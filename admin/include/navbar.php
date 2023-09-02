0eb0cf9578db59bcd63c7f0c0b16025efe7a1e73
mohamed <mr319242@gmail.com>
2023-09-02 09:57:51 +0300

    publish

---
+++ admin/include/navbar.php
@@ -0,0 +1,111 @@
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+        </li>
+      </ul>
+
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="main.php?dir=dashboard&page=dashboard" class="brand-link">
+        <span class="brand-text font-weight-light"> نظام الحضور والغياب </span>
+        <!--<img src="uploads/logo.png" alt="AdminLTE Logo" class="brand-image elevation-3" style="box-shadow: none;">-->
+      </a>
+
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+            <!-- <img src="uploads/logo.png" class="img-circle elevation-2" alt="User Image"> -->
+          </div>
+          <div class="info">
+            <a href="#" class="d-block"> <?php echo $_SESSION['admin_username']; ?> </a>
+          </div>
+        </div>
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+            <li class="nav-item">
+              <a href="main.php?dir=dashboard&page=dashboard" class="nav-link">
+                <p>
+                  الرئيسية
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <p>
+                  الجهات الرئيسية
+                  <i class="right fas fa-angle-left"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="main.php?dir=banners&page=report" class="nav-link">
+                    <p> مشاهدة الكل </p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="main.php?dir=about_us&page=report" class="nav-link">
+                    <p> اضافة جهه جديدة </p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <p>
+                  الأقسام
+                  <i class="right fas fa-angle-left"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="main.php?dir=categories&page=report" class="nav-link">
+                    <p> جميع الأقسام </p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <p>
+                  المنتجات
+                  <i class="fas fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                <li class="nav-item">
+                  <a href="main.php?dir=products&page=report" class="nav-link">
+                    <p> جميع المنتجات </p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="main.php?dir=products&page=add" class="nav-link">
+                    <p> اضافة منتج </p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a href="logout" class="nav-link" style="color: #e74c3c;">
+                <p>
+                  تسجيل خروج
+                </p>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
+    </aside>
